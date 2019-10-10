dd3eb62d08ba1df3bcfec0472385a202f691e261
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-10 00:09:10 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books ORDER BY year LIMIT 3;"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters ORDER BY MOTTO LIMIT 1;"
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
@@ -16,9 +16,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.id FROM series JOIN characters ON series.author_id = characters.author_id;"
+  # "SELECT series.title FROM series JOIN characters ON series.author_id = characters.author_id WHERE characters.species = 'human' GROUP BY series.title LIMIT 1;"
+  "SELECT series.title FROM series JOIN characters ON series.author_id = characters.author_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(Characters.id) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.id) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.id) DESC;"
 end
